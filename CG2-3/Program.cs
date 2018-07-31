cf63f18078d9f7a6b99f96331610a0a61c3b7c85
breelindsey <35509667+breelindsey@users.noreply.github.com>
2018-07-30 22:27:32 -0500

    I don't know why there are changes...

--- CG2-3/Program.cs
+++ CG2-3/Program.cs
@@ -6,7 +6,17 @@ namespace CG2_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Name");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter a Past-Tense Verb");
+            string Verb = Console.ReadLine();
+
+            Console.WriteLine("Enter a Noun");
+            string Noun = Console.ReadLine();
+
+            Console.WriteLine(Name + " be nimble " + Name + " be quick " + Name + " " + Verb + " over the " + Noun + ".");
+            Console.ReadLine();
         }
     }
 }
